d32de978d4f49faa44d215580262d389ba0f4f37
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2023-11-25 21:33:19 -0300

    FEAT: Primeiro Commit

---
+++ src/Conta.java
@@ -0,0 +1,38 @@
+public abstract class Conta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = Conta.SEQUENCIAL++;
+    }
+
+    public void depositar(double valor){
+
+    }
+
+    public void sacar(double valor){
+
+    }
+
+    public void transferir(double valor, Conta contaDestino){
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
